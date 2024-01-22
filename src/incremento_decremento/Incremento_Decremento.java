27a342a636f7ac2c2d7a2ba3e25e64731726b704
JUAN CARLOS <JUAN CARLOS@LAPTOP-NF1JBM48>
2024-01-22 11:50:26 -0600

    Arimetricos clase del dia lunes 22 de enero

---
+++ src/incremento_decremento/Incremento_Decremento.java
@@ -0,0 +1,17 @@
+
+package incremento_decremento;
+
+public class Incremento_Decremento {
+
+   
+    public static void main(String[] args) {
+     
+        int x=5,y;
+        y=++x;//INCREMENTO EN 1
+        //y=x++;
+        System.out.println(y);
+        System.out.println(x);
+        
+    }
+    
+}
