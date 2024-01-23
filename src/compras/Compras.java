f266461ff576fff56cd6a9ef0e8f69e4d2ae6a54
JUAN CARLOS <JUAN CARLOS@LAPTOP-NF1JBM48>
2024-01-22 18:07:48 -0600

    Arimetricos clase del dia lunes 22 de enero

--- src/compras/Compras.java
+++ src/compras/Compras.java
@@ -6,18 +6,20 @@ public class Compras {
 
     public static void main(String[] args) {
         // DECLARACION  VARIABLES
-        double precio, cantidad, subtotal,descuento, documento, total,total1;
-        
-        
+        double precio, cantidad, subtotal,descuento, total,total1; 
+        //Pedimos por teclado cuantos articulos va a comprar
         Scanner entrada = new Scanner(System.in);
         System.out.println("Cuanto vas a comprar");
+        //pedimos por teclado para saber cual va ha ser el precio
         cantidad = entrada.nextDouble();
         System.out.println("Cual es el precio");
-        //Calcular descuento
+        //pedimos por teclado el descuento que se va a aplicar
         precio = entrada.nextDouble();
         System.out.println("escribir el descuento en decimales:");
         descuento=entrada.nextDouble();
+        //calculamos el subtotal
         subtotal=cantidad*precio;
+        
         total1=subtotal*descuento;
         total=subtotal-total1;
         
